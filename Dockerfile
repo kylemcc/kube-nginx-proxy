f447cd9612d92c361539493a588dae1ed2ed9b47
Kyle McCullough <kylemcc@gmail.com>
2019-02-12 16:03:40 -0600

    add -host flag to kube-gen

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM nginx:1.14.2
 LABEL maintainer="Kyle McCullough <kylemcc@gmail.com>"
 
-LABEL version="0.2.0"
+LABEL version="0.2.1"
 
 # Install available package updates, wget, and install/updates certificates
 RUN apt-get update \
--- start-kube-gen.sh
+++ start-kube-gen.sh
@@ -2,4 +2,4 @@
 
 set -e
 sleep 1
-kube-gen -watch -type pods -type services -wait 2s:10s -post-cmd 'nginx -s reload' /app/sites.tmpl /etc/nginx/conf.d/sites.conf
+kube-gen -watch -type pods -type services -wait 2s:10s -post-cmd 'nginx -s reload' -host http://localhost:8001 /app/sites.tmpl /etc/nginx/conf.d/sites.conf
